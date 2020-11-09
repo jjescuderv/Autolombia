ddf6a441522d289f9ca0372e02879a0c200ec93a
DESKTOP-SPNOEQI <jjescuderv@eafit.edu.co>
2020-11-09 04:53:42 -0500

    Archivos base de Lang

---
+++ resources/lang/en/app.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /** Información del index */
+    'name' => 'Autolombia',
+    'welcome' => 'Find your vehicle',
+    'welcome_msg' => 'Used cars for sale',
+    'location_title' => 'Location',
+    'location_city' => 'Medellín, Colombia',
+    'location_address' => 'Arboleda del Rodeo, 2622',
+    'social_media' => 'Around the web',
+    'about_title' => 'About',
+    'about_content' => 'Autolombia is great!',
+    'copyright' => 'Copyright © Autolombia 2020',
+
+    /** Información del menú */
+    'register' => 'Register',
+    'login' => 'Login',
+    'store' => 'Store',
+    'auctions' => 'Auctions',
+    'profile' => 'Profile',
+    'logout' => 'Logout',
+    'admin' => 'Admin',
+    'others_service' => 'Service team',
+
+    /** Información de la base de datos */
+    'dbempty' => 'The database is empty!'
+
+];
---
+++ resources/lang/es/app.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /** Información del index */
+    'name' => 'Autolombia',
+    'welcome' => 'Encuentra tu vehículo',
+    'welcome_msg' => 'Autos usados a la venta',
+    'location_title' => 'Ubicación',
+    'location_city' => 'Medellín, Colombia',
+    'location_address' => 'Arboleda del Rodeo, 2622',
+    'social_media' => 'En las redes',
+    'about_title' => 'Sobre nosotros',
+    'about_content' => 'Autolombia es una chimba!',
+    'copyright' => 'Copyright © Autolombia 2020',
+
+    /** Información del menú */
+    'register' => 'Registrarse',
+    'login' => 'Ingresar',
+    'store' => 'Tienda',
+    'auctions' => 'Subastas',
+    'profile' => 'Perfil',
+    'logout' => 'Salir',
+    'admin' => 'Admin',
+    'others_service' => 'Servicio',
+
+];
