2544180dac6022c78af5b6d5e8fb4be53f024671
asperezm <47333056+asperezm@users.noreply.github.com>
2020-11-10 15:57:16 -0500

    Mejora de vistas

--- resources/views/admin/auction/show_all.blade.php
+++ resources/views/admin/auction/show_all.blade.php
@@ -18,11 +18,26 @@
                                 <div class="col-2"> 
                                     {{ $auction->getId() }} 
                                 </div>
-                            
-                                <div class="col-7">
-                                    {{ $auction->car->getColor() . " " . $auction->car->getBrand() . " " . $auction->car->getModel()}} <br/>
-                                    {{ $auction->getBeginning() . " to " .  $auction->getEnding() }} <br/>
-                                    Reserve price: {{ $auction->getReservePrice() }}
+
+                                <div class="col-lg-4" style="float:center">
+                                    <img src="{{ asset('storage/' . $auction->car->getImagePath()) }}" 
+                                    style="height: 100%; width: 200%; object-fit: contain;">
+                                </div>
+
+                                <div class="col-lg-8" style="float:left">
+                                    <h3 class="card-title"> {{ $auction->car->getBrand() . ' ' .
+                                        $auction->car->getModel() }} </h3>
+                                    <h5> {{ $auction->car->getColor() }} </h5>
+                                    @if($auction->bids->max('bid_value') == NULL)
+                                    <h4> Reserve price: {{ '$' . $auction->getReservePrice()}} </h4>
+                                    @else
+                                    <h4> Highest bid: {{ '$' . $auction->bids->max('bid_value') }} </h4>
+                                    @endif
+                                    <p class="card-text"> {{ __( $auction->getBeginning() . " to " .
+                                        $auction->getEnding()) }} </p>
+                                    <i class="fas fa-tachometer-alt"
+                                        style="font-size:25px; float:left; margin: 0 10px 0 0"></i> {{
+                                    $auction->car->getMileage() . ' miles'}} 
                                 </div>
 
                                 <div class="col-3">
